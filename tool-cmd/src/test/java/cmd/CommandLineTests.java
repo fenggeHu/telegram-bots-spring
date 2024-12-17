869176dc82e574b9d112ed54538d4d85a2e0d229
max.hu <pbfoxhu@gmail.com>
2024-12-17 19:05:44 +0800

    update

--- tool-cmd/src/test/java/cmd/CommandLineTests.java
+++ tool-cmd/src/test/java/cmd/CommandLineTests.java
@@ -10,11 +10,11 @@ public class CommandLineTests {
 
     @Test
     public void testCmdParser() {
-        String line = "grep -rn -F \"Corba Notify\" ptnlog.log |grep \"msgObj|Name--\"|awk '{print $8}'|sort|uniq";
+        String line = "grep -rn -F \"Corba Notify\" main.log |grep \"msgObj|Name--\"|awk '{print $8}'|sort|uniq";
         var cmds = CommandLine.parseAll(line);
 
-        String line2 = "grep -rn -F 'Corba Notify \"' ptnlog.log |grep 'msgObj|Name--\\|'|awk '{print $8}'|sort|uniq";
+        String line2 = "grep -rn -F 'Corba Notify \"' main.log |grep 'msgObj|Name--\\|'|awk '{print $8}'|sort|uniq";
         var cmds2 = CommandLine.parseAll(line2);
-        System.out.println();
+        System.out.println(cmds2);
     }
 }
